c05aa7c03061164c96f206ff547fd711b4306164
Hollerwöger Niklas <Niklas.HOLLERWOEGER@htlvb.at>
2022-12-12 09:15:34 +0100

    Solve Day 01 Part  1 sample

--- AoC_FirstDay/Program.cs
+++ AoC_FirstDay/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string sample = @"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+int maxCalories = sample.Split("\n\n")
+    .Select(elf => elf.Split("\n")
+    .Select(int.Parse)
+    .Sum()
+    )
+    .Max();
+Console.WriteLine($"Part 1: {maxCalories}");
+    
